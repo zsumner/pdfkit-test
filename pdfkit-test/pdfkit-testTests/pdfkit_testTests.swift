3158062394357805e47aafa75b610a0e35b7bbdf
Zachary Sumner <zachary@bluetread.com>
2017-11-15 11:01:32 -0500

    initial project commit

---
+++ pdfkit-test/pdfkit-testTests/pdfkit_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  pdfkit_testTests.swift
+//  pdfkit-testTests
+//
+//  Created by Zachary Sumner on 11/15/17.
+//  Copyright © 2017 Zachary Sumner. All rights reserved.
+//
+
+import XCTest
+@testable import pdfkit_test
+
+class pdfkit_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
